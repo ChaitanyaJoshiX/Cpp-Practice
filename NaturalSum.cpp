19df8509e15598d3abdd7d9da92760d4c6be5027
Chaitanya Joshi <75534893+ChaitanyaJoshiX@users.noreply.github.com>
2022-01-01 22:44:17 +0530

    Add files via upload

---
+++ NaturalSum.cpp
@@ -0,0 +1,22 @@
+/*
+WAP to print first n Natural numbers and their sum.
+Take n as input.
+*/
+#include <iostream>
+using namespace std;
+main()
+{
+  int lim,i = 1;
+  int sum = 0;
+  cout << "Enter n terms : ";
+  cin >> lim;
+
+  do // Using a do while loop to excecute till counter reaches limit.
+  {
+    cout << i << endl;
+    sum += i;
+    i++;
+  } while(i <= lim);
+
+  cout << "Sum = " << sum << endl;
+}
